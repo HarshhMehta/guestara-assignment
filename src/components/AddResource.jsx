8245cb1b294ffbf96a221b2c511168a9019c3b9d
Harsh Mehta <92149953+HarshhMehta@users.noreply.github.com>
2025-02-07 19:32:36 +0530

    Update  aleart box in add resources

--- src/components/AddResource.jsx
+++ src/components/AddResource.jsx
@@ -1,18 +1,22 @@
 import { Plus } from "lucide-react"
 
 const AddResource = ({ onAddResource }) => {
+  const handleAddResource = () => {
+    onAddResource();
+    alert("Resource added successfully!");
+  };
+
   return (
     <div className="absolute z-20 right-25 top-2 cursor-pointer flex items-center justify-between p-4">
       <button
         className="px-4 cursor-pointer py-2 bg-blue-500 text-white rounded hover:bg-blue-600 flex items-center"
-        onClick={onAddResource}
+        onClick={handleAddResource}
       >
         <Plus className="w-4 h-4 mr-2" />
         Add Resource
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default AddResource
-
+export default AddResource;
